29e7453899343f36b0ca4c1835779a2aeb30bc7d
amy-1989 <>
2023-10-03 14:22:02 +0000

    Add style to leaderboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,51 @@
-import { myQuestions } from './questions.js';
+import { myQuestions } from "./questions.js";
 
-const playButton = document.getElementById('playBtn');
-const instructionButton = document.getElementById('instructionBtn');
-const nextButton = document.getElementById('nextBtn');
-const saveScoreButton = document.getElementById('saveBtn');
-const welcomePage = document.getElementById('popUp');
-const quizArea = document.getElementById('quizArea');
-const questionElement = document.getElementById('question');
-const answerButtonsElement = document.getElementById('answerBtns');
-const leaderboardButton = document.getElementById('scoresBtn');
-const instructionNote = document.getElementById('instructionNote');
-const welcome = document.getElementById('text');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
-const username = document.getElementById('username');
-const leaderboard = document.getElementById('leaderboard');
-const result = document.getElementById('endGameResult');
-let scoreElement = parseInt(document.getElementById('score').innerText);
-const highScoreList = document.getElementById('highScoresList');
+const playButton = document.getElementById("playBtn");
+const instructionButton = document.getElementById("instructionBtn");
+const nextButton = document.getElementById("nextBtn");
+const saveScoreButton = document.getElementById("saveBtn");
+const welcomePage = document.getElementById("popUp");
+const quizArea = document.getElementById("quizArea");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answerBtns");
+const leaderboardButton = document.getElementById("scoresBtn");
+const instructionNote = document.getElementById("instructionNote");
+const welcome = document.getElementById("text");
+const username = document.getElementById("username");
+const leaderboard = document.getElementById("leaderboard");
+const result = document.getElementById("endGameResult");
+let scoreElement = parseInt(document.getElementById("score").innerText);
+const highScoreList = document.getElementById("highScoresList");
 const maxHighScores = 3;
 const highScoreModal = document.getElementById("myModal");
 
-// constt score = {
-// //     score: scoreElement,
-// //     name: username.value
-// //}
-
 let score = {};
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-console.log(highScores);
-
-
-username.addEventListener('keyup', () => {
+username.addEventListener("keyup", () => {
     saveScoreButton.disabled = !username.value;
 });
-
 
 let randomiseQuestions;
 let currentQuestionIndex;
 
-playButton.addEventListener('click', beginQuiz);
-instructionButton.addEventListener('click', instructions);
-saveScoreButton.addEventListener('click', function (e) {
+playButton.addEventListener("click", beginQuiz);
+instructionButton.addEventListener("click", instructions);
+saveScoreButton.addEventListener("click", function (e) {
     saveHighScore(e);
-    highScoreModal.classList.add('hide');
+    highScoreModal.classList.add("hide");
 });
 
-leaderboardButton.addEventListener('click', displayLeaderboard);
+leaderboardButton.addEventListener("click", displayLeaderboard);
 /**
-* to increment the questions and load the next question when next button is clicked
-*/
-nextButton.addEventListener('click', () => {
+ * to increment the questions and load the next question when next button is clicked
+ */
+nextButton.addEventListener("click", () => {
     currentQuestionIndex++;
     if (currentQuestionIndex < 10) {
         setNextQuestion();
-        localStorage.setItem('score', scoreElement);
+        localStorage.setItem("score", scoreElement);
         console.log(scoreElement);
         console.log(score);
     } else {
@@ -65,25 +55,25 @@ nextButton.addEventListener('click', () => {
 
 /**
  *  * begin the quiz game
-*/
+ */
 function beginQuiz() {
-    console.log('Quiz started');
+    console.log("Quiz started");
     //to hide welcome page and display the quiz game section
-    welcomePage.classList.add('hide');
-    quizArea.classList.remove('hide');
+    welcomePage.classList.add("hide");
+    quizArea.classList.remove("hide");
+    //shuffles the questions array to generate random questions
     shuffleArray(myQuestions);
     currentQuestionIndex = 0;
     scoreElement = 0;
-    document.getElementById('score').innerText = 0;
+    document.getElementById("score").innerText = 0;
     setNextQuestion();
-};
+}
 
 /**
-* to randomise the questions
-*/
+ * to randomise the questions
+ */
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
@@ -91,49 +81,49 @@ function shuffleArray(array) {
 }
 
 /**
-* resets question area and loads the next random question
-*/
+ * resets question area and loads the next random question
+ */
 function setNextQuestion() {
     resetState();
-    // randomiseQuestions = shuffleArray(myQuestions);
     displayQuestion(myQuestions[currentQuestionIndex]);
     console.log(myQuestions);
 }
 
 /**
-* display the question
-*/
+ * display the question
+ */
 function displayQuestion(questions) {
     questionElement.innerText = questions.question;
 
     //display the answers
     shuffleArray(questions.answers);
-    questions.answers.forEach(answer => {
-
+    questions.answers.forEach((answer) => {
         //creates the answer buttons
-        let button = document.createElement('button');
+        let button = document.createElement("button");
         button.innerText = answer.text;
-        button.classList.add('answerButton');
+        button.classList.add("answerButton");
 
         //check if the answer selected is correct
         if (answer.correct) {
             button.dataset.correct = answer.correct;
         }
-        button.addEventListener('click', selectAnswer);
+        button.addEventListener("click", selectAnswer);
         answerButtonsElement.appendChild(button);
 
         //disable the buttons after user makes a choice
-        answerButtonsElement.addEventListener('click', () => button.disabled = true,
-            nextButton.classList.remove('hide'));
+        answerButtonsElement.addEventListener(
+            "click",
+            () => (button.disabled = true),
+            nextButton.classList.remove("hide")
+        );
     });
-
 }
 
 /**
  * resets the question and answer area
  */
 function resetState() {
-    nextButton.classList.add('hide');
+    nextButton.classList.add("hide");
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
@@ -145,12 +135,12 @@ function selectAnswer(e) {
 
     //to increment the score for correct answers
     if (correct) {
-        document.getElementById('score').innerText = ++scoreElement;
+        document.getElementById("score").innerText = ++scoreElement;
         console.log(scoreElement);
         console.log(score);
     }
     //convert answer buttons into an array
-    Array.from(answerButtonsElement.children).forEach(button => {
+    Array.from(answerButtonsElement.children).forEach((button) => {
         setStatusCLass(button, button.dataset.correct);
     });
 }
@@ -161,46 +151,43 @@ function selectAnswer(e) {
 function setStatusCLass(element, correct) {
     clearStatusCLass(element);
     if (correct) {
-        element.classList.add('correct');
+        element.classList.add("correct");
     } else {
-        element.classList.add('wrong');
+        element.classList.add("wrong");
     }
 }
 /**
  * removes previous class selections of correct or wrong
  */
 function clearStatusCLass(element) {
-    element.classList.remove('correct');
-    element.classList.remove('wrong');
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
 }
 /**
  * to display the instructions page
  */
 function instructions() {
-    leaderboardButton.classList.add('hide');
-    instructionButton.classList.add('hide');
-    welcome.classList.add('hide');
-    instructionNote.classList.remove('hide');
+    leaderboardButton.classList.add("hide");
+    instructionButton.classList.add("hide");
+    welcome.classList.add("hide");
+    instructionNote.classList.remove("hide");
     console.log(instructions);
 }
 /**
  * to display the end final score page
  */
 function endGame() {
-    welcomePage.classList.remove('hide');
-    quizArea.classList.add('hide');
-    playButton.innerText = 'Play Again';
-    playButton.classList.remove('hide');
-    instructionNote.classList.add('hide');
-    welcome.classList.add('hide');
-    instructionButton.classList.add('hide');
-    leaderboardButton.classList.remove('hide');
-    leaderboard.classList.add('hide');
-    console.log('endpage function');
+    welcomePage.classList.remove("hide");
+    quizArea.classList.add("hide");
+    playButton.innerText = "Play Again";
+    playButton.classList.remove("hide");
+    instructionNote.classList.add("hide");
+    welcome.classList.add("hide");
+    instructionButton.classList.add("hide");
+    leaderboardButton.classList.remove("hide");
+    leaderboard.classList.add("hide");
     result.innerText = `Well Done! You have scored ${scoreElement}!`;
-    highScoreModal.classList.remove('hide');
-    console.log(scoreElement);
-    console.log(score);
+    highScoreModal.classList.remove("hide");
 }
 /**
  * to save your score to the leaderboard
@@ -209,7 +196,7 @@ function saveHighScore(e) {
     e.preventDefault();
     score = {
         score: scoreElement,
-        name: username.value
+        name: username.value,
     };
 
     highScores.push(score);
@@ -220,11 +207,13 @@ function saveHighScore(e) {
 
     highScores.splice(3);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores));
+    localStorage.setItem("highScores", JSON.stringify(highScores));
 
-    highScoreList.innerHTML = highScores.map(score => {
-        return `<li class="highScore">${score.name}-${score.score}`;
-    }).join();
+    highScoreList.innerHTML = highScores
+        .map((score) => {
+            return `<li class="highScore">${score.name} - ${score.score}`;
+        })
+        .join();
     console.log(score.name);
     console.log(score.score);
 }
@@ -233,10 +222,12 @@ function saveHighScore(e) {
  * to display the leaderboard
  */
 function displayLeaderboard() {
-    leaderboard.classList.remove('hide');
-    instructionButton.classList.add('hide');
-    leaderboardButton.classList.add('hide');
-    highScoreList.innerHTML = highScores.map(score => {
-        return `<li class="highScore">${score.name}-${score.score}`;
-    }).join();
+    leaderboard.classList.remove("hide");
+    instructionButton.classList.add("hide");
+    leaderboardButton.classList.add("hide");
+    highScoreList.innerHTML = highScores
+        .map((score) => {
+            return `<li class="highScore">${score.name} - ${score.score}`;
+        })
+        .join();
 }
