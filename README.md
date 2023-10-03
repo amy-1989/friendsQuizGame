2e961f3dc78ec6397d90b98cb0ea8cdc0e9f0fee
amy-1989 <>
2023-10-03 20:39:37 +0000

    fix validation errors

--- README.md
+++ README.md
@@ -1,40 +1,46 @@
 # Friends Quiz Game
-This is a Trivia Quiz Game for fans of the popular TV show Friends. 
+
+This is a Trivia Quiz Game for fans of the popular TV show Friends.
 
 ## User Experience
 
 - User Stories
-  
+
   - First time visitor Goals
+
     - As a first time visitor I want to learn how to play the game.
     - As a first time visitor I want to navigate easily through the game.
     - As a first time visitor I want to be able to see my score.
-  
+
   - Returning Visitor Goals
+
     - As a returning visitor I want to be able to answer different questions.
     - As a returning visitor I want to be able to see my progress/score and compare previous playthroughs.
     - As a returning visitor I want to be able to bypass instructional sections.
-  
+
   - Frequent Visitor Goals
     - As a returning visitor I want to be able to play new questions.
     - As a returning visitor I want to be able to see how my scores compare to other users.
 
 - Design
-  
+
   - Colour Scheme
+
     - The colour scheme was chosen to compliment the theme of the quiz. Colours were taken from the Friends TV show colour palette, with consideration taken for accessibility. The main colours used are black, purple & yellow.
-  
+
   - Typography
+
     - The Caveat font from Google Fonts was chosen with Sans-Serif as a backup. This was chosen due to its similarity to the Friends logo and is also easy to read.
-  
+
   - Imagery
     - As this is a quiz game, imagery has been kept simple with the use of a single Friends Tv Show logo to convey the theme of the quiz game.
-  
+
 - Wireframes
-  
+
   ![Image of wireframe for mobile & desktop devices](./documents/images/friends-wireframe.webp)
 
 ## Features
+
 - Responsive on a range of devices
 - Interactive Elements
 - Leaderboard to track high scores
@@ -42,6 +48,7 @@ This is a Trivia Quiz Game for fans of the popular TV show Friends.
 ## Technologies Used
 
 ### Languages Used
+
 -HTML
 -CSS
 -Javascript
@@ -55,61 +62,89 @@ This is a Trivia Quiz Game for fans of the popular TV show Friends.
 2. Git
 
 - Git was used for version control by utilizing the Gitpod terminal to commit to Git & push to GitHub.
-  
+
 3. GitHub
 
 - GitHub is used to store the projects code after being pushed from Git.
-  
+
 4. Balsamiq
 
 - Balsamiq was used to create the wireframes during the design process.
 
 ## Testing
 
+The W3C Markup Validator and W3C CSS Validator Services were used to validate every page of the project to ensure there were no syntax errors.
+
+JSHint was used to validate the javascript files. No major issues were found.
+
+- W3C Markup Validator
+  ![image of validator results](./documents/images/)
+
+- W3C CSS Validator
+  ![image of validator results](./documents/images/css-validation.webp)
+
 ### Testing User Stories from the User Experience (UX) Section
 
 - First time visitor Goals
-  
+
   - As a first time visitor I want to learn how to play the game.
-  
+
     - Upon entering the site users arrive on a landing page with a clearly marked instruction button. Once clicked this will display how to play instructions.
-  
+
   - As a first time visitor I want to navigate easily through the game.
-   
-    - The landing page has been designed with simplicity and ease of use in mind. Users are greeted with three buttons clearly labelled Play, Instructions & Leaderboard. 
+
+    - The landing page has been designed with simplicity and ease of use in mind. Users are greeted with three buttons clearly labelled Play, Instructions & Leaderboard.
     - If users click for instructions the instruction page will display along with a button to play, allowing the user to jump right into the game without having to navigate backwards. The quiz section has clearly displayed questions and answers and a next button to clearly guide the user through the game.
     - After playing the game users will be greeted with the option to play again also.
-  
+
   - As a first time visitor I want to be able to see my score.
-  
+
     - There is a score counter at the bottom of the game area where players can easily see their score as they progress through the game.
     - At the end of the game users are greeted with a message showing their score and an option to add their name to the leaderboard in the event of a high score.
-  
+
   - Returning Visitor Goals
-  
+
     - As a returning visitor I want to be able to answer different questions.
-  
+
       - The game has been designed with 10 questions pulled from a collection of 50 questions to allow for variety upon subsequent playthroughs.
-  
+
     - As a returning visitor I want to be able to see my progress/score and compare previous playthroughs.
-     
+
       - Upon finishing the game users are given the option of saving their high score to the leaderboard. Upon entering the site, users can click the leaderboard button on the landing page to view their high scores.
-  
+
     - As a returning visitor I want to be able to bypass instructional sections.
       - On the landing page the instructions are hidden behind an instruction button, allowing repeat players to begin playing straight away.
-  
+
   - Frequent Visitor Goals
-  
+
     - As a returning visitor I want to be able to play new questions.
-      - The game has been designed with an array of 50 questions from which 10 are pulled. These questions are stored in a separate js file to allow to easily update the array with more questions as needed to keep the game fresh. 
-  
+
+      - The game has been designed with an array of 50 questions from which 10 are pulled. These questions are stored in a separate js file to allow to easily update the array with more questions as needed to keep the game fresh.
+
     - As a returning visitor I want to be able to see how my scores compare to other users.
       - There is a leaderboard function which allows users to add their name to the scoreboard. Different users of the same device will be able to compare scores, as scores are saved to local storage.
 
 ### Further Testing
+- The website was tested on Google Chrome, Microsoft Edge, Safari and Mozilla Firefox browsers.
+  ![image of browser test chart](./documents/images/browser-test.webp)
 
+- The website was tested on a variety of devices such as Desktop, Ipad, Iphone and Galaxy phones.
+  ![image of responsiveness chart](./documents/images/responsiveness-test.webp)
+
+- A number of testing was done to ensure that all buttons were working.
+- Friends and family were asked to play the game on their own devices, and point out any bugs or issues.
+- The website was continuously tested & debugged throughout development using Chrome DevTools.
+- The website was tested for Desktop and Mobile performance using Chrome DevTools Lighthouse
+  - Desktop Lighthouse Result
+    ![image of lighthouse result for desktop](./documents/images/lighthouse-desktop.webp)
+  - Mobile Lighthouse Result
+    ![image of lighthouse result for mobile](./documents/images/lighthouse-mobile.webp)
+- The site was tested for accessability using WAVE
+    ![image of WAVE result](./documents/images/wave-test.webp)
 
 ### Known Bugs
+
+- On desktop, if a user clicks in the area around the answer buttons, but ot on an answer itself, the browser disables the answer buttons so the user cannot score a point for that question.
 
 ## Deployment
 
@@ -122,7 +157,7 @@ At the top of the Repository (not top of page), locate the "Settings" Button on 
 Scroll down the Settings page until you locate the "GitHub Pages" Section.
 Under "Source", click the dropdown called "None" and select "Main", then "Save".
 The page will automatically refresh.
-The now published site link  will be at the top of the "GitHub Pages" section.
+The now published site link will be at the top of the "GitHub Pages" section.
 
 ### Forking the GitHub Repository
 
@@ -145,8 +180,13 @@ Press Enter. Your local clone will be created.
 ## Credits
 
 ### Code
+- The code for the overall quiz was inspired by WebDevSimplified tutorial
+  https://www.youtube.com/watch?v=riDzcEQbX6k&ab_channel=WebDevSimplified
+- The code for saving the high score was inspired by youtube tutorial by Brian Design
+  https://www.youtube.com/watch?v=f4fB9Xg2JEY&ab_channel=BrianDesign 
+- The code for local storage was inspired by 
 
-  
+
 ### Content
 
 - All content was written by the developer.
@@ -154,5 +194,7 @@ Press Enter. Your local clone will be created.
 
 ### Media
 
-
+- Copyright of the Friends logo belongs to David & Marta Kauffman & WarnerMedia
+- The favicon image was taken from Very Ireland.
+- 
 ### Acknowledgements
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 import { myQuestions } from "./questions.js";
 
-/**the code for this quiz was inspired by 
+/**the code for this quiz was inspired by
  * WebDevSimplified youtube tutorial
  */
 
@@ -31,11 +31,11 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 let currentQuestionIndex;
 
 /**
- * 'keyup' Event Listener will prevent user from being able to save a score 
+ * 'keyup' Event Listener will prevent user from being able to save a score
  * without a username
  */
 username.addEventListener("keyup", () => {
-    saveScoreButton.disabled = !username.value;
+  saveScoreButton.disabled = !username.value;
 });
 
 /**
@@ -43,15 +43,15 @@ username.addEventListener("keyup", () => {
  * playButton will start the game
  * instructionButton will display how to play the game
  * saveScoreButton will save the scores to leaderboard
- * leaderboard will display the high scores 
+ * leaderboard will display the high scores
  */
 playButton.addEventListener("click", beginQuiz);
 
 instructionButton.addEventListener("click", instructions);
 
 saveScoreButton.addEventListener("click", function (e) {
-    saveHighScore(e);
-    highScoreModal.classList.add("hide");
+  saveHighScore(e);
+  highScoreModal.classList.add("hide");
 });
 
 leaderboardButton.addEventListener("click", displayLeaderboard);
@@ -60,208 +60,207 @@ leaderboardButton.addEventListener("click", displayLeaderboard);
  * to increment the questions and load the next question when next button is clicked
  */
 nextButton.addEventListener("click", () => {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < 10) {
-        setNextQuestion();
-        localStorage.setItem("score", scoreElement);
-    } else {
-        endGame();
-    }
+  currentQuestionIndex++;
+  if (currentQuestionIndex < 10) {
+    setNextQuestion();
+    localStorage.setItem("score", scoreElement);
+  } else {
+    endGame();
+  }
 });
 
 /**
  * begins the quiz game
  */
 function beginQuiz() {
-    console.log("Quiz started");
+  console.log("Quiz started");
 
-    //to hide welcome page and display the quiz game section
-    welcomePage.classList.add("hide");
-    quizArea.classList.remove("hide");
+  //to hide welcome page and display the quiz game section
+  welcomePage.classList.add("hide");
+  quizArea.classList.remove("hide");
 
-    //shuffles the questions array to generate random questions
-    shuffleArray(myQuestions);
+  //shuffles the questions array to generate random questions
+  shuffleArray(myQuestions);
 
-    //resets the current question index and scores
-    currentQuestionIndex = 0;
-    scoreElement = 0;
-    document.getElementById("score").innerText = 0;
-    setNextQuestion();
+  //resets the current question index and scores
+  currentQuestionIndex = 0;
+  scoreElement = 0;
+  document.getElementById("score").innerText = 0;
+  setNextQuestion();
 }
 
 /**
  * to randomise the questions and answers
- * Fisher-Yates algorithm provided by mentor 
+ * Fisher-Yates algorithm provided by mentor
  */
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 /**
  * resets question area and loads the next question
  */
 function setNextQuestion() {
-    resetState();
-    displayQuestion(myQuestions[currentQuestionIndex]);
-    console.log(myQuestions);
+  resetState();
+  displayQuestion(myQuestions[currentQuestionIndex]);
+  console.log(myQuestions);
 }
 
 /**
  * displays the question
  */
 function displayQuestion(questions) {
-    questionElement.innerText = questions.question;
+  questionElement.innerText = questions.question;
 
-    //shuffle the answers so that the top answer is not always correct
-    shuffleArray(questions.answers);
+  //shuffle the answers so that the top answer is not always correct
+  shuffleArray(questions.answers);
 
-    //displays the answers
-    questions.answers.forEach((answer) => {
+  //displays the answers
+  questions.answers.forEach((answer) => {
+    //creates the answer buttons
+    let button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("answerButton");
 
-        //creates the answer buttons
-        let button = document.createElement("button");
-        button.innerText = answer.text;
-        button.classList.add("answerButton");
+    //check if the answer selected is correct
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
 
-        //check if the answer selected is correct
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-        answerButtonsElement.appendChild(button);
-
-        //disable the buttons after user makes a choice
-        answerButtonsElement.addEventListener(
-            "click",
-            () => (button.disabled = true),
-            nextButton.classList.remove("hide")
-        );
-    });
+    //disable the buttons after user makes a choice
+    answerButtonsElement.addEventListener(
+      "click",
+      () => (button.disabled = true),
+      nextButton.classList.remove("hide")
+    );
+  });
 }
 
 /**
  * resets the question and answer area
  */
 function resetState() {
-    nextButton.classList.add("hide");
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-    }
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
 }
 /**
- * 
- * @param {event} e 
- * incements the score in the event of a selected answer 
+ *
+ * @param {event} e
+ * incements the score in the event of a selected answer
  * being correct
  */
 function selectAnswer(e) {
-    const selectedButton = e.target;
-    const correct = selectedButton.dataset.correct;
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
 
-    //to increment the score for correct answers
-    if (correct) {
-        document.getElementById("score").innerText = ++scoreElement;
-        console.log(scoreElement);
-        console.log(score);
-    }
-    //convert answer buttons into an array
-    Array.from(answerButtonsElement.children).forEach((button) => {
-        setStatusCLass(button, button.dataset.correct);
-    });
+  //to increment the score for correct answers
+  if (correct) {
+    document.getElementById("score").innerText = ++scoreElement;
+    console.log(scoreElement);
+    console.log(score);
+  }
+  //convert answer buttons into an array
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusCLass(button, button.dataset.correct);
+  });
 }
 
 /**
  * to add styles dependent on selected correct or incorrect answer
  */
 function setStatusCLass(element, correct) {
-    clearStatusCLass(element);
-    if (correct) {
-        element.classList.add("correct");
-    } else {
-        element.classList.add("wrong");
-    }
+  clearStatusCLass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
 }
 /**
  * removes previous class selections of correct or wrong
  */
 function clearStatusCLass(element) {
-    element.classList.remove("correct");
-    element.classList.remove("wrong");
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
 }
 /**
  * to display the instructions page
  */
 function instructions() {
-    leaderboardButton.classList.add("hide");
-    instructionButton.classList.add("hide");
-    welcome.classList.add("hide");
-    instructionNote.classList.remove("hide");
-    console.log(instructions);
+  leaderboardButton.classList.add("hide");
+  instructionButton.classList.add("hide");
+  welcome.classList.add("hide");
+  instructionNote.classList.remove("hide");
+  console.log(instructions);
 }
 
 /**
  * to display the end final score page
  */
 function endGame() {
-    welcomePage.classList.remove("hide");
-    quizArea.classList.add("hide");
-    playButton.innerText = "Play Again";
-    playButton.classList.remove("hide");
-    instructionNote.classList.add("hide");
-    welcome.classList.add("hide");
-    instructionButton.classList.add("hide");
-    leaderboardButton.classList.remove("hide");
-    leaderboard.classList.add("hide");
+  welcomePage.classList.remove("hide");
+  quizArea.classList.add("hide");
+  playButton.innerText = "Play Again";
+  playButton.classList.remove("hide");
+  instructionNote.classList.add("hide");
+  welcome.classList.add("hide");
+  instructionButton.classList.add("hide");
+  leaderboardButton.classList.remove("hide");
+  leaderboard.classList.add("hide");
 
-    //to display the modal
-    result.innerText = `Well Done! You have scored ${scoreElement}!`;
-    highScoreModal.classList.remove("hide");
+  //to display the modal
+  result.innerText = `Well Done! You have scored ${scoreElement}!`;
+  highScoreModal.classList.remove("hide");
 }
 /**
  * to save your score to the leaderboard
- * the code for this function was inspired by this tutorial 
+ * the code for this function was inspired by this tutorial
  *  https://www.youtube.com/watch?v=f4fB9Xg2JEY&ab_channel=BrianDesign
- * and this article 
+ * and this article
  * https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68
  */
 function saveHighScore(e) {
-    e.preventDefault();
-    score = {
-        score: scoreElement,
-        name: username.value,
-    };
+  e.preventDefault();
+  score = {
+    score: scoreElement,
+    name: username.value,
+  };
 
-    highScores.push(score);
+  highScores.push(score);
 
-    highScores.sort((a, b) => {
-        return b.score - a.score;
-    });
+  highScores.sort((a, b) => {
+    return b.score - a.score;
+  });
 
-    highScores.splice(3);
+  highScores.splice(3);
 
-    localStorage.setItem("highScores", JSON.stringify(highScores));
+  localStorage.setItem("highScores", JSON.stringify(highScores));
 
-    highScoreList.innerHTML = highScores
-        .map((score) => {
-            return `<li class="highScore">${score.name} - ${score.score}`;
-        })
-        .join();
+  highScoreList.innerHTML = highScores
+    .map((score) => {
+      return `<li class="highScore">${score.name} - ${score.score}`;
+    })
+    .join();
 }
 
 /**
  * to display the leaderboard
  */
 function displayLeaderboard() {
-    leaderboard.classList.remove("hide");
-    instructionButton.classList.add("hide");
-    leaderboardButton.classList.add("hide");
-    highScoreList.innerHTML = highScores
-        .map((score) => {
-            return `<li class="highScore">${score.name} - ${score.score}`;
-        })
-        .join();
+  leaderboard.classList.remove("hide");
+  instructionButton.classList.add("hide");
+  leaderboardButton.classList.add("hide");
+  highScoreList.innerHTML = highScores
+    .map((score) => {
+      return `<li class="highScore">${score.name} - ${score.score}`;
+    })
+    .join();
 }
